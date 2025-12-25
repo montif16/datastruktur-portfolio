4517dce78eac412174204e3f26f2d6c385dcc33a
montif16 <Lukas1532@hotmail.com>
2025-12-25 05:30:16 +0100

    Assignment

--- dynamicarray/dynamicarray.js
+++ dynamicarray/dynamicarray.js
@@ -7,9 +7,37 @@ export default class DynamicArray {
     if (!Number.isInteger(capacity) || capacity < 1) {
       throw new RangeError("Capacity must be a positive integer");
     }
-
-    this._data = new StaticArray(capacity); 
+    this._data = new StaticArray(capacity);
+    this._size = 0;
   }
+
+  
+
+  _isInt(n) {
+    return Number.isInteger(n);
+  }
+
+  _throwRange() {
+    throw new RangeError("Index out of bounds");
+  }
+
+  
+  _checkAccessIndex(index) {
+    if (!this._isInt(index) || index < 0) this._throwRange();
+    if (index >= this.capacity()) this._throwRange(); 
+    if (index >= this._size) this._throwRange();      
+  }
+
+
+  _checkInsertIndex(index) {
+    if (!this._isInt(index) || index < 0) this._throwRange();
+
+
+    if (index > this.capacity()) this._throwRange();
+    if (index > this._size) this._throwRange();
+  }
+
+  // ----- required methods -----
 
   size() {
     return this._size;
@@ -17,5 +45,65 @@ export default class DynamicArray {
 
   capacity() {
     return this._data.length();
+  }
+
+  get(index) {
+    this._checkAccessIndex(index);
+    return this._data.get(index);
+  }
+
+  set(index, item) {
+    this._checkAccessIndex(index);
+    this._data.set(index, item);
+  }
+
+  add(item) {
+    if (this._size >= this.capacity()) {
+      this.grow();
+    }
+    this._data.set(this._size, item);
+    this._size++;
+  }
+
+  grow() {
+    const oldCap = this.capacity();
+    const newCap = oldCap * 2; 
+
+    const newData = new StaticArray(newCap);
+    for (let i = 0; i < this._size; i++) {
+      newData.set(i, this._data.get(i));
+    }
+    this._data = newData;
+  }
+
+  insert(index, item) {
+    this._checkInsertIndex(index);
+
+    if (this._size >= this.capacity()) {
+      this.grow();
+    }
+
+    for (let i = this._size; i > index; i--) {
+      this._data.set(i, this._data.get(i - 1));
+    }
+
+    this._data.set(index, item);
+    this._size++;
+  }
+
+  remove(index) {
+    this._checkAccessIndex(index);
+
+    for (let i = index; i < this._size - 1; i++) {
+      this._data.set(i, this._data.get(i + 1));
+    }
+
+    this._data.set(this._size - 1, undefined);
+    this._size--;
+  }
+
+  clear() {
+
+    this._size = 0;
   }
 }
